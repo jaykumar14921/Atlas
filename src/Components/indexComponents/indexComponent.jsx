604c67b5d6096bc799ca2535a1eb8fcbdb762bb0
Your Namejaykumar14921@gmail.comjay <you@example.com>
2025-11-03 02:26:36 +0530

    Initial commit

---
+++ src/Components/indexComponents/indexComponent.jsx
@@ -0,0 +1,14 @@
+import './indexComponent.css';
+// import { NavBarComponent } from '../navBarComponents/navBarComponent';
+import {CodeEditor} from '../resultComponents/resultComponent';
+export function IndexComponent(){
+     return (
+  <>
+    <section>
+      {/* <NavBarComponent></NavBarComponent> */}
+      <CodeEditor></CodeEditor>
+    </section>
+  </>
+);
+
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { IndexComponent } from './Components/indexComponents/indexComponent';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <IndexComponent />
   </React.StrictMode>
 );
 
