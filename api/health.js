35e5b8360877c09c096470ab360d7818fc961cb4
Your Namejaykumar14921@gmail.comjay <you@example.com>
2026-04-17 02:44:28 +0530

    roll-back

--- api/health.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.json({
-    status: 'ok',
-    hasApiKey: !!process.env.ANTHROPIC_API_KEY,
-  });
-}
